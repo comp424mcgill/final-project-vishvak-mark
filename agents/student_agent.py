442b7af3b442dca11c3c117548721ae6c606b03b
vishvak11 <vishvakraghavan@gmail.com>
2022-04-05 15:43:58 -0400

    Fixed errors in student_agent.py

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -24,7 +24,7 @@ class StudentAgent(Agent):
         }
         self.moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
 
-    def check_valid_step(self, start_pos, end_pos, adv_pos, chess_board, max_step):
+    def check_valid_step(self, start_pos1, end_pos1, adv_pos1, chess_board, max_step):
         """
         Check if the step the agent takes is valid (reachable and within max steps).
 
@@ -45,18 +45,60 @@ class StudentAgent(Agent):
         if np.array_equal(start_pos, end_pos):
             return True
         """
-
-        x_diff = abs(start_pos[0] - end_pos[0])
-        y_diff = abs(start_pos[1] - end_pos[1])
-        if (x_diff + y_diff <= max_step):
-            return True
-        else:
+        # checks if shortest distance is reachable
+        # does not check whether the path is reachable when factoring in borders
+        x_diff = abs(start_pos1[0] - end_pos1[0])
+        y_diff = abs(start_pos1[1] - end_pos1[1])
+        if (x_diff + y_diff > max_step):
             return False
+        start_pos = np.asarray(start_pos1)
+        end_pos = np.asarray(end_pos1)
+        adv_pos = np.asarray(adv_pos1)
+        if np.array_equal(start_pos, end_pos):
+            return True
+
+        #check if path is actually reachable
+        state_queue = [(start_pos, 0)]
+        visited = {tuple(start_pos)}
+        is_reached = False
+        while state_queue and not is_reached:
+            # print("hi")
+            # print(state_queue)
+            cur_pos, cur_step = state_queue.pop(0)
+            r, c = cur_pos
+            if cur_step == max_step:
+                break
+            for dir, move in enumerate(self.moves):
+                # print("hi3")
+                # print(dir, move)
+                if chess_board[r, c, dir]:
+                    continue
+
+                next_pos = cur_pos + move
+                # print("hi2")
+                # print(next_pos)
+                if np.array_equal(next_pos, adv_pos) or tuple(next_pos) in visited:
+                    continue
+                if np.array_equal(next_pos, end_pos):
+                    is_reached = True
+                    break
+                visited.add(tuple(next_pos))
+                state_queue.append((next_pos, cur_step + 1))
+        return is_reached
 
     def dist(self, c1, c2):
+        # print(type(c1))
         x_dist = c1[0] - c2[0]
         y_dist = c1[1] - c2[1]
         return (x_dist, y_dist)
+
+    def barrier_chooser(self, chess_board, end_pos, guess):
+        if (not chess_board[end_pos[0], end_pos[1], guess]):
+            return guess
+        for i in range(4):
+            if (not chess_board[end_pos[0], end_pos[1], i]):
+                dir = i
+                return dir
 
     def step(self, chess_board, my_pos, adv_pos, max_step):
         """
@@ -97,8 +139,8 @@ class StudentAgent(Agent):
                     #print(coord)
                     poss_moves.append(coord)
         min = 100
-        final = 0
-        print(best_moves)
+        final = []
+        #print(best_moves)
         for move in poss_moves:
             #print(move)
             for move2 in best_moves:
@@ -109,16 +151,27 @@ class StudentAgent(Agent):
                     final = move
         dis = self.dist(final, adv_pos)
         if (abs(dis[0]) <= abs(dis[1])):
-            if (dis[1] < 0):
+            if (dis[1] < 0 and not chess_board[final[0], final[1], 1]):
                 dir = 1
-            else:
+            elif (dis[1] >= 0 and not chess_board[final[0], final[1], 3]):
                 dir = 3
-        else:
-            if (dis[0] < 0):
-                dir = 2
             else:
+                if (dis[0] < 0):
+                    dir = self.barrier_chooser(chess_board, final, 2)
+                else:
+                    dir = self.barrier_chooser(chess_board, final, 0)
+
+        else:
+            if (dis[0] < 0 and not chess_board[final[0], final[1], 2]):
+                dir = 2
+            elif (dis[0] >= 0 and not chess_board[final[0], final[1], 0]):
                 dir = 0
+            else:
+                if (dis[1] < 0):
+                    dir = self.barrier_chooser(chess_board, final, 1)
+                else:
+                    dir = self.barrier_chooser(chess_board, final, 3)
 
-
-
+        # print(final)
+        # print(dir)
         return final, dir
